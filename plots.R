2371c379832b6a54d32d3e1a53ec711208d1cd58
McCartneyAC <acm9q@virginia.edu>
2018-04-25 13:29:43 -0400

    updates

--- plots.R
+++ plots.R
@@ -10,14 +10,35 @@ killins %>%
   group_by(year) %>% 
   count(shooting_type) %>% 
   ggplot(aes(x = year, y = n, fill = fct_reorder(shooting_type, -n))) + 
-  geom_col() + 
+  geom_col(color="black") + 
   coord_flip()+
   scale_fill_brewer(palette="Set3") +
   labs(
-    title = "Fatalities from School Shootings Since Columbine",
+    title = "School Shootings Since Columbine",
     x = "Year",
     y = "Count of Shootings",
     fill = "Shooting Type",
     caption = "Data Via Washington Post"
   ) + 
-  theme_few()
+  theme_few()+
+  guides(color=FALSE)
+
+killins %>%
+  group_by(shooting_type) %>% 
+  summarize(killed = sum(killed)) %>% 
+  mutate(shooting_type = fct_reorder(shooting_type, -killed)) %>% 
+  filter(killed >= 1) %>% 
+  ggplot(aes(x = shooting_type, y = killed, fill = shooting_type)) +
+  geom_col(color="black") +
+  coord_flip() +
+  scale_fill_brewer(palette="Set3")+
+  labs(
+    title="Fatalities by Shooting Type",
+    y = "Fatalities",
+    x = "",
+    caption = "Data Via Washington Post"
+  ) + 
+  theme_few() +
+  guides(fill = FALSE)
+
+summary(lm(killed~year, data = killins))
